7cf3218979e150f9f21730ea302777c328b73143
Rosmayani <you@example.com>
2020-01-22 11:47:52 +0700

    belajar2

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('hallo', function()
     .'Laravel Memang Keren';
 });
 
-//buat 5 buah profil pribadi
+//Tugas buat 5 buah profil pribadi
 Route::get('profil/rosma', function()
 {
     return 'Halo nama saya Rosmayani<br>'
@@ -45,10 +45,78 @@ Route::get('biodata/{nama}', function($a)
     return 'Halo Ini Biodata Saya '.$a;
 });
 
-//Route Parameter Tugas
+//Tugas Route Parameter
 Route::get('biodata/{nama}/{alamat}/{umur}', function($a,$b,$c)
 {
     return 'Nama Saya '.$a.
            '<br>Alamat Saya di '.$b.
            '<br>Umur Saya '.$c.' tahun';
+});
+
+//Route Optional Parameter
+Route::get('name/{param?}', function($a="Kenedy")
+{
+    return 'Your Name Is '.$a;
+});
+
+//Tugas Route Optional Parameter
+Route::get('pesan/{a?}/{b?}/{c?}', function ($eat=null, $drink=null, $hrg=null)
+{
+    if (isset($eat)) {
+        $eat = "Anda Memesan $eat";
+        echo $eat;
+    }
+    if (isset($drink)) {
+        $drink = " & $drink";
+        echo $drink;
+    }
+    if (isset($hrg)) {
+        $hrg = " dengan harga Rp $hrg";
+        echo $hrg;
+    }
+    if (!$eat && !$drink){
+    return "Anda Belum Memesan Sesuatu";
+    }
+    if (isset($hrg)) {
+        $size = " dengan harga Rp ".$hrg;
+        echo "$size";
+        if ($hrg <= 0) {
+            echo " Harga Tidak Valid";
+        }
+        elseif ($hrg >= 25000) {
+            echo "dapat size medium";
+        }
+        elseif ($hrg > 25000) {
+            echo "dapat size small";
+        }
+    }
+});
+
+//Tugas Optional Parameter
+Route::get('tni/{nama?}/{berat?}/{umur?}', function($nama=null, $bb=null, $umur=null)
+{
+    if (isset($nama)) {
+        $nama = "Nama Anda : $nama <br>";
+        echo $nama;
+    }
+    if (isset($bb)) {
+        $v = " Berat Badan Anda : $bb kg<br>";
+        echo $v;
+    }
+    if (isset($umur)) {
+        $umur = " Umur Anda : $umur tahun<br>";
+        echo $umur;
+    }
+    if (isset($bb)) {
+    
+        if ($bb >= 76) {
+            echo " Anda Harus Turun Berat Badan";
+        }
+        else if ($bb >= 100) {
+            echo "Berat Badan Anda Ideal";
+        }
+        else if ($bb >= 50) {
+            echo "Naikkan Berat Badan Anda";
+        }
+    }
 });
